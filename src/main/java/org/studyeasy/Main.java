f31142330b25a48e97830a53ab02f6df40546380
Flávio Santana <f.o.santana@gmail.com>
2025-12-06 16:26:59 -0300

    Excpetion implementado

--- src/main/java/org/studyeasy/Main.java
+++ src/main/java/org/studyeasy/Main.java
@@ -5,9 +5,15 @@ package org.studyeasy;
 public class Main {
     public static void main(String[] args) {
 
-        Iphone iphone = new Iphone();
-
-        System.out.println(iphone.processador());
+        try{
+            System.out.println("Before Expeption");
+            System.out.println("This divide by 8 excption"+10/0);
+            System.out.println("After Expeption");
+        }catch (ArithmeticException e){
+            System.out.println("Arithmetic Exception");
+        }catch (Exception e){
+            System.out.println("Expeption");
+        }
 
     }
 }
